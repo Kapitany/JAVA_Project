211ddbe68051f25d8050cac46cfe8a17cb9b445c
Salfer Alex Valentin <salfer.alex@gmail.com>
2015-09-25 14:04:35 +0200

    projekt rendezve

--- FileSystem/FileSystem_tester/src/filesystem_tester/FileSystem_tester.java
+++ FileSystem/FileSystem_tester/src/filesystem_tester/FileSystem_tester.java
@@ -8,10 +8,19 @@ import java.util.ArrayList;
  */
 public class FileSystem_tester {
 
-    private final ArrayList <Subject> list = new ArrayList<>();
+    private final ArrayList <Subject> subjectList = new ArrayList<>();
+    
+     private void subjectListLoader() {
+         
+     }
+    
+    private void start() {
+        subjectListLoader();
+    }
     
     public static void main(String[] args) {
-        
+        FileSystem_tester tester = new FileSystem_tester();
+        tester.start();
     }
     
 }
