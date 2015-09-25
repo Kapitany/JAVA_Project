f9b134ada73635390302e046156b25a63872c308
Salfer Alex Valentin <salfer.alex@gmail.com>
2015-09-25 13:57:40 +0200

    Subject osztály

--- FileSystem/FileSystem_tester/src/filesystem_tester/Subject.java
+++ FileSystem/FileSystem_tester/src/filesystem_tester/Subject.java
@@ -5,5 +5,37 @@ package filesystem_tester;
  * @author Tomega
  */
 class Subject {
-    
+    private final String subjactName;
+    private final String subjactCode;
+    private final int creditValue;
+    private final String[] requirements;
+    private final String subjactType;
+
+    public Subject(String subjactName, String subjactCode, int creditValue, String[] requirements, String subjactType) {
+        this.subjactName = subjactName;
+        this.subjactCode = subjactCode;
+        this.creditValue = creditValue;
+        this.requirements = requirements;
+        this.subjactType = subjactType;
+    }
+
+    public String getSubjactName() {
+        return subjactName;
+    }
+
+    public String getSubjactCode() {
+        return subjactCode;
+    }
+
+    public int getCreditValue() {
+        return creditValue;
+    }
+
+    public String[] getRequirements() {
+        return requirements;
+    }
+
+    public String getSubjactType() {
+        return subjactType;
+    }
 }
