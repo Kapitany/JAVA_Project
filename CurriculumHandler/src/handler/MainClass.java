9bd54ef7baed039c9c88e24f34d025f2d31fcb18
Salfer Alex Valentin <salfer.alex@gmail.com>
2015-11-04 22:24:45 +0100

    applikációvá alakítva, a tesztelés kedvéért most nem a főpanellel dolgozva még

--- CurriculumHandler/src/handler/MainClass.java
+++ CurriculumHandler/src/handler/MainClass.java
@@ -1,16 +1,26 @@
 package handler;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author tomega
  */
-
 //A kikommentezett sorok egyenként is sok sor kimenettel rendelkeznek. Szemléltető jellegűek, a tesztelést szolgálják/ták.
+public class MainClass extends Application {
 
-public class MainClass {
-    
     @SuppressWarnings("CallToPrintStackTrace")
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    @SuppressWarnings("CallToPrintStackTrace")
+    public void start(Stage primaryStage) throws Exception {
         try {
             LogicalCurriculumHandler handler = new LogicalCurriculumHandler();
             handler.list();
@@ -18,11 +28,14 @@ public class MainClass {
 //            loader.listSubjects();
 //            DependenceViewer viewer = new DependenceViewer(loader.getSubjectList());
             GraphFileHandler graphHandler = new GraphFileHandler(loader.getSubjectList());
-            
         } catch (Exception ex) {
             ex.printStackTrace();
             System.err.println("Initialisation aborted!");
         }
+        Parent root = FXMLLoader.load(getClass().getResource("graphView.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-    
+
 }
