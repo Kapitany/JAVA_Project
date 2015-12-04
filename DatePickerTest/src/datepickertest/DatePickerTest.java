0d0d2d9545ee7f5d2797b0a6f91af2be23eddd77
Kapusi Ottilia <sirah00000@gmail.com>
2015-12-04 15:06:06 +0100

    boldog szülinapot

--- DatePickerTest/src/datepickertest/DatePickerTest.java
+++ DatePickerTest/src/datepickertest/DatePickerTest.java
@@ -9,8 +9,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.DatePicker;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -21,22 +19,14 @@ public class DatePickerTest extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
+       
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
-        DatePicker calendar = new DatePicker();
-        
-        StackPane root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        root.getChildren().addAll(calendar);
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
     
 }
--- DatePickerTest/src/datepickertest/FXMLDocumentController.java
+++ DatePickerTest/src/datepickertest/FXMLDocumentController.java
@@ -6,11 +6,18 @@
 package datepickertest;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.MonthDay;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.util.Callback;
 
 /**
  *
@@ -20,16 +27,55 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    DatePicker datepicker;
+    ComboBox<String> combobox;
+     
+    public FXMLDocumentController(){
+        
     }
+    
+    private void handleButtonAction(ActionEvent event) {
+        //naptári bejegyzések tárolóból kiszedni az aktuális napra vonatkozó adatokat
+        
+    }
+    
+    final Callback<DatePicker, DateCell> dayCellFactory = new Callback<DatePicker, DateCell>() {
+        public DateCell call(final DatePicker datePicker) {
+            return new DateCell() {
+                @Override public void updateItem(LocalDate item, boolean empty) {
+                    super.updateItem(item, empty);
+
+                    if (MonthDay.from(item).equals(MonthDay.of(9, 25))) {
+                        setTooltip(new Tooltip("Happy Birthday!"));
+                        setStyle("-fx-background-color: #ff4444;");
+                    }
+                    if (item.equals(LocalDate.now().plusDays(1))) {
+                        // Tomorrow is too soon.
+                        setDisable(true);
+                    }
+                }
+            };
+        }
+    };
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        System.out.println("initialize...");
+        
+        //combobox.getItems().addAll("Day", "Week");
+        
+        
+        datepicker.setDayCellFactory(dayCellFactory);
+        
+        
+        datepicker.setValue(LocalDate.of(2015, 12, 8));
+        datepicker.setValue(LocalDate.of(2015, 12, 10));
+        datepicker.setTooltip(new Tooltip("Tooltip"));
+
+        datepicker.setShowWeekNumbers(true);
+        //datepicker.show();
+        datepicker.requestFocus();
     }    
     
 }
