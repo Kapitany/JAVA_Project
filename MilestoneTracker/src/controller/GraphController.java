aed6ac7efcca7db5076bc20fa808605ba63d3997
Kapusi Ottilia <sirah00000@gmail.com>
2015-12-05 19:08:04 +0100

    vhogy kinéz végre

--- MilestoneTracker/src/controller/GraphController.java
+++ MilestoneTracker/src/controller/GraphController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
@@ -36,35 +37,64 @@ public class GraphController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
+        pane.setStyle("-fx-border-color:#ff4444");
+
         int numSemesters = Launcher.getGraphHandler().getNumSemesters();
-        for (int i = 0; i < numSemesters; i++) {
+        for (int i = 0; i < numSemesters/2+1; i++) {
             ColumnConstraints column = new ColumnConstraints();
-            column.setPercentWidth(100/numSemesters);
-            RowConstraints rc = new RowConstraints();
+            column.setPercentWidth(100/(numSemesters/2+1));
             pane.getColumnConstraints().add(column);
         }
-        RowConstraints rc = new RowConstraints(30);
-        pane.getRowConstraints().add(0, rc);
-        for (int i = 0; i < numSemesters; i++) {
-            pane.add(new Label((i + 1) + ". félév"), i, 0);
-        }
+        /*RowConstraints rc = new RowConstraints(100);
+        pane.getRowConstraints().add(0, rc);*/
+        
         
         ArrayList<ArrayList<Subject>> subjectsPerSemester = Launcher.getGraphHandler().getGraphContainer();
 
         for (int semester = 0; semester < numSemesters; semester++) {
             VBox tmpBox = new VBox();
+            Label semesterCount = new Label((semester + 1) + ". félév");
+            semesterCount.setStyle("-fx-border-color:#44ff44");
+            tmpBox.getChildren().add(semesterCount);
+            
             for (int subj = 0; subj < subjectsPerSemester.get(semester).size(); subj++) {
+                Label lblSubjetName;
 
-                Text lblSubjetName = new Text(subjectsPerSemester.get(semester).get(subj).getSubjectName());
+                String tempName = subjectsPerSemester.get(semester).get(subj).getSubjectName();
+                //lblSubjetName = new Text(tempName);
+                //lblSubjetName.setStyle("-fx-border-color:#000000");
+                if(tempName.length() >= 25){
+                    String [] tempNameArray = tempName.split(" ");
+                    lblSubjetName = new Label();
+                    /*tempName = tempNameArray[0];
+                    for(int i = 1; i < tempNameArray.length; i++){
+                        tempName += "\n"+tempNameArray[i];
+                    }*/
+                    //lblSubjetName.setStyle("-fx-border-color:#000000");
+                    lblSubjetName.setText(tempName);
+                    lblSubjetName.setEllipsisString(tempName);
+                }else{
+                    lblSubjetName = new Label(tempName);
+                }
+//                Label lblSubjetName = new Label("Alma");
                 tmpBox.getChildren().add(lblSubjetName);
+                
                 tmpBox.setAlignment(Pos.CENTER);
-                tmpBox.setStyle("-fx-border-color:#000000");
+                //tmpBox.setStyle("-fx-border-color:#44ff44");
                 
             }
-            pane.add(tmpBox, semester, 1);
-        }
 
+            System.out.println(tmpBox.isResizable());
+            //tmpBox.resizeRelocate(0, 0, 600, 600);
+            System.out.println(tmpBox.getHeight());
+            if(semester <= (numSemesters/2)){
+                pane.add(tmpBox, semester, 0);
+            }else{
+                pane.add(tmpBox, semester-(numSemesters/2)-1, 1);
+            }
+        }
+        
         pane.setAlignment(Pos.CENTER_LEFT);
     }
 }
